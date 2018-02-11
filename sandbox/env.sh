3a5d19b475a541a3146d3afd16c0e4e4103a1f91
Pawel Witek <wiciu_666@tlen.pl>
2018-02-11 16:24:44 +0100

    draft file props

---
+++ sandbox/env.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+export test_opt="test2"
---
+++ sandbox/script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+source "/home/pawel/source/sandbox/env.sh"
+echo "tada"
+echo $test_opt
+
+su -m pawel -c "/home/pawel/source/sandbox/execute"
